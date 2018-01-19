0c88384da66d1c6a1e14de036516b3af32a57572
Surya <surya3997@gmail.com>
2018-01-19 18:58:33 +0530

    database creator working properly

--- database_creator.php
+++ database_creator.php
@@ -6,12 +6,110 @@ try {
     $mng = new MongoDB\Driver\Manager("mongodb://localhost:27017");
     
     $bulk = new MongoDB\Driver\BulkWrite;
+	date_default_timezone_set('Asia/Kolkata');
     
-    //$doc = ['_id' => new MongoDB\BSON\ObjectID, 'name' => 'Benz', 'price' => 26700];
-    //$bulk->insert($doc);
-    //$bulk->update(['name' => 'Benz'], ['$set' => ['price' => 52000]]);
-    $bulk->delete(['name' => 'Toyota']);
+	$mongo_date = new DateTime('1997-09-03');
+	$birth_date = $mongo_date->getTimestamp();	
+
+	$mongo_date = new DateTime();
+	$reg_date = $mongo_date->getTimestamp();
+
+	$mongo_date = new DateTime('2017-10-03  16:30:00');
+	$notify_time = $mongo_date->getTimestamp();
+
+	$mongo_date = new DateTime('2017-10-03  17:40:00');
+	$post_time = $mongo_date->getTimestamp();
+
+	$mongo_date = new DateTime('2017-10-03  17:40:00');
+	$comment_time = $mongo_date->getTimestamp();
+
+	$mongo_date = new DateTime('2017-10-03  17:40:00');
+	$feedback_time = $mongo_date->getTimestamp();
+
+	$mongo_date = new DateTime('2017-10-03  17:40:00');
+	$chat_time = $mongo_date->getTimestamp();
+
+	$secrets = 	[
+					"question_1" => "What is your pets name?", 
+					"answer_1" => "Tom", 
+					"question_2" => "What's your mother tongue?", 
+					"answer_2" => "Telugu"
+				];
+
+	$user_inf = [
+					"school" => "ssv", 
+					"college" => "PSG", 
+					"relationship_status" => "single", 
+					"mobile" => "9791745977", 
+					"mobile_visibility" => "public", 
+					"website" => "www.surya3997.ml", 
+					"connector_id" => "surya3997", 
+					"home_town" => "cbe", 
+					"current_city" => "cbe", 
+					"Job" => ""
+				];
+
+
+    $doc = 	[
+				'_id' => new MongoDB\BSON\ObjectID, 
+				'name' => 'Surya Prasath', 
+				'email' => "surya3997@gmail.com", 
+				'password' => "1d2727face9e8a5171fd434ca9ac155020be70db", 
+				'gender' => "male", 
+				'birthday' => $birth_date, 
+				"registered" => $reg_date, 
+				"secret_quote" => $secrets, 
+				"user_info" => $user_inf, 
+				"user_cover_pic" => [], 
+				"user_profile_pic" => [], 
+				"notification" => 	[
+										[
+											"notify_txt" => "You have signed in!", 
+											"notify_time" => $notify_time,
+											"seen" => 0
+										]
+									],
+				"user_status" => 1,
+				"user_post" => 	[
+									[
+										"post_txt" => "This is my first post!",
+										"post_pic" => "",
+										"post_time" => $post_time,
+										"visibility" => "friends",
+										"likes" => 	[],
+										"comments" => 	[
+															[
+																"commenter" => "",
+																"comment_txt" => "",
+																"time" => ""
+															]
+														]
+									]
+								],
+				"friends" => 	[],
+				"feedback" => 	[
+									[
+										"feedback_txt" => "",
+										"star" => 0,
+										"time" => ""
+									]
+								],
+				"warning" => 	[
+									[
+										"warning_txt" => "",
+										"seen" => 1
+									]
+								],
+				"group_chat" => [
+									[
+										"chat_text" => "",
+										"time" => $chat_time
+									]
+								]
+
+			];
     
+	$bulk->insert($doc);
     $mng->executeBulkWrite('testdb.cars', $bulk);
         
 } catch (MongoDB\Driver\Exception\Exception $e) {
